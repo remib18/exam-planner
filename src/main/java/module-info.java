cff211d09a7d6755de42ce0bf033ab3dd99938a7
Rémi Bernard <remib18@outlook.fr>
2022-11-25 00:52:34 +0100

    Project refactoring

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,11 @@
-module fr.univtours.examplanner.examplanner {
+module fr.univtours.examplanner {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires org.jetbrains.annotations;
 	requires commons.csv;
 
-
 	opens fr.univtours.examplanner to javafx.fxml;
 	exports fr.univtours.examplanner;
+	exports fr.univtours.examplanner.ui;
+	opens fr.univtours.examplanner.ui to javafx.fxml;
 }
