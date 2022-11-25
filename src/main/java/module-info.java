5b3c93ed8b263ef503225c57c09317a3134d1cef
Rémi Bernard <remib18@outlook.fr>
2022-11-25 13:35:12 +0100

    Adding java.sql

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module fr.univtours.examplanner {
 	requires javafx.fxml;
 	requires org.jetbrains.annotations;
 	requires commons.csv;
+	requires java.sql;
 
 	opens fr.univtours.examplanner to javafx.fxml;
 	exports fr.univtours.examplanner;
