17e3a9dfebdc915b7ecc0b4fe2fd0f500d7a78fa
Rémi Bernard <remib18@outlook.fr>
2022-11-30 10:30:17 +0100

    Reformat files

--- src/main/java/fr/univtours/examplanner/entities/dtos/DepartmentDTO.java
+++ src/main/java/fr/univtours/examplanner/entities/dtos/DepartmentDTO.java
@@ -10,69 +10,69 @@ import java.util.Objects;
 
 public class DepartmentDTO extends WithIDEntity {
 
-    /**
-     * Maquettes gérées par le département
-     */
-    @NotNull
-    private final List<MockUpDTO> MockUpDTOs = new ArrayList<>();
-    /**
-     * Nom du département
-     */
-    @NotNull
-    private String name;
+	/**
+	 * Maquettes gérées par le département
+	 */
+	@NotNull
+	private final List<MockUpDTO> MockUpDTOs = new ArrayList<>();
+	/**
+	 * Nom du département
+	 */
+	@NotNull
+	private String name;
 
-    public DepartmentDTO(@Nullable String id, @NotNull String name) {
-        super(id);
-        this.name = name;
-    }
+	public DepartmentDTO(@Nullable String id, @NotNull String name) {
+		super(id);
+		this.name = name;
+	}
 
-    public @NotNull String getName() {
-        return name;
-    }
+	public @NotNull String getName() {
+		return name;
+	}
 
-    public void setName(@NotNull String name) {
-        this.name = name;
-    }
+	public void setName(@NotNull String name) {
+		this.name = name;
+	}
 
-    public @NotNull List<MockUpDTO> getMockUpDTOs() {
-        return MockUpDTOs;
-    }
+	public @NotNull List<MockUpDTO> getMockUpDTOs() {
+		return MockUpDTOs;
+	}
 
-    public void addMockUpDTO(@NotNull MockUpDTO MockUpDTO) {
-        this.MockUpDTOs.add(MockUpDTO);
-    }
+	public void addMockUpDTO(@NotNull MockUpDTO MockUpDTO) {
+		this.MockUpDTOs.add(MockUpDTO);
+	}
 
-    public void addMockUpDTO(@NotNull List<MockUpDTO> MockUpDTOs) {
-        this.MockUpDTOs.addAll(MockUpDTOs);
-    }
+	public void addMockUpDTO(@NotNull List<MockUpDTO> MockUpDTOs) {
+		this.MockUpDTOs.addAll(MockUpDTOs);
+	}
 
-    public void removeMockUpDTO(@NotNull MockUpDTO MockUpDTO) {
-        this.MockUpDTOs.remove(MockUpDTO);
-    }
+	public void removeMockUpDTO(@NotNull MockUpDTO MockUpDTO) {
+		this.MockUpDTOs.remove(MockUpDTO);
+	}
 
-    public void removeMockUpDTO(@NotNull List<MockUpDTO> MockUpDTOs) {
-        this.MockUpDTOs.removeAll(MockUpDTOs);
-    }
+	public void removeMockUpDTO(@NotNull List<MockUpDTO> MockUpDTOs) {
+		this.MockUpDTOs.removeAll(MockUpDTOs);
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        DepartmentDTO that = (DepartmentDTO) o;
-        return Objects.equals(id, that.id);
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		DepartmentDTO that = (DepartmentDTO) o;
+		return Objects.equals(id, that.id);
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, MockUpDTOs);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name, MockUpDTOs);
+	}
 
-    @Override
-    public String toString() {
-        return "DepartmentDTO{" +
-                "\n\tid: " + id +
-                ", \n\tname: " + name +
-                ", \n\tMockUpDTOs: " + MockUpDTOs +
-                "\n}";
-    }
+	@Override
+	public String toString() {
+		return "DepartmentDTO{" +
+				"\n\tid: " + id +
+				", \n\tname: " + name +
+				", \n\tMockUpDTOs: " + MockUpDTOs +
+				"\n}";
+	}
 }
--- src/main/java/fr/univtours/examplanner/entities/dtos/MockUpDTO.java
+++ src/main/java/fr/univtours/examplanner/entities/dtos/MockUpDTO.java
@@ -12,104 +12,104 @@ import java.util.Objects;
 // Maquette ou cursus
 public class MockUpDTO extends WithIDEntity {
 
-    /**
-     * Nom de la maquette
-     */
-    @NotNull
-    private final String name;
-    /**
-     * Matières de la maquette
-     */
-    @NotNull
-    private final List<SubjectDTO> subjects = new ArrayList<>();
-    /**
-     * Niveau de la maquette
-     */
-    @NotNull
-    private Degree degree;
-    /**
-     * Semestre de la maquette
-     */
-    private int semester;
+	/**
+	 * Nom de la maquette
+	 */
+	@NotNull
+	private final String name;
+	/**
+	 * Matières de la maquette
+	 */
+	@NotNull
+	private final List<SubjectDTO> subjects = new ArrayList<>();
+	/**
+	 * Niveau de la maquette
+	 */
+	@NotNull
+	private Degree degree;
+	/**
+	 * Semestre de la maquette
+	 */
+	private int semester;
 
-    public MockUpDTO(
-            @Nullable String id,
-            @NotNull String name,
-            @NotNull Degree degree,
-            int semester,
-            @NotNull List<SubjectDTO> subjects
-    ) {
-        super(id);
-        this.name = name;
-        this.degree = degree;
-        setSemester(semester);
-        this.subjects.addAll(subjects);
-    }
+	public MockUpDTO(
+			@Nullable String id,
+			@NotNull String name,
+			@NotNull Degree degree,
+			int semester,
+			@NotNull List<SubjectDTO> subjects
+	) {
+		super(id);
+		this.name = name;
+		this.degree = degree;
+		setSemester(semester);
+		this.subjects.addAll(subjects);
+	}
 
-    public @NotNull String getName() {
-        return name;
-    }
+	public @NotNull String getName() {
+		return name;
+	}
 
-    public @NotNull Degree getDegree() {
-        return degree;
-    }
+	public @NotNull Degree getDegree() {
+		return degree;
+	}
 
-    public void setDegree(@NotNull Degree degree) {
-        this.degree = degree;
-    }
+	public void setDegree(@NotNull Degree degree) {
+		this.degree = degree;
+	}
 
-    public int getSemester() {
-        return semester;
-    }
+	public int getSemester() {
+		return semester;
+	}
 
-    public void setSemester(int semester) throws IllegalArgumentException {
-        if (semester < 1) {
-            throw new IllegalArgumentException("Semester must be greater than 0");
-        }
-        this.semester = semester;
-    }
+	public void setSemester(int semester) throws IllegalArgumentException {
+		if (semester < 1) {
+			throw new IllegalArgumentException("Semester must be greater than 0");
+		}
+		this.semester = semester;
+	}
 
-    public @NotNull List<SubjectDTO> getSubjects() {
-        return subjects;
-    }
+	public @NotNull List<SubjectDTO> getSubjects() {
+		return subjects;
+	}
 
-    public void addSubject(@NotNull SubjectDTO subject) {
-        this.subjects.add(subject);
-    }
+	public void addSubject(@NotNull SubjectDTO subject) {
+		this.subjects.add(subject);
+	}
 
-    public void addSubject(@NotNull List<SubjectDTO> subjects) {
-        this.subjects.addAll(subjects);
-    }
+	public void addSubject(@NotNull List<SubjectDTO> subjects) {
+		this.subjects.addAll(subjects);
+	}
 
-    public void removeSubject(@NotNull SubjectDTO subject) {
-        this.subjects.remove(subject);
-    }
+	public void removeSubject(@NotNull SubjectDTO subject) {
+		this.subjects.remove(subject);
+	}
 
-    public void removeSubject(@NotNull List<SubjectDTO> subjects) {
-        this.subjects.removeAll(subjects);
-    }
+	public void removeSubject(@NotNull List<SubjectDTO> subjects) {
+		this.subjects.removeAll(subjects);
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        MockUpDTO mockup = (MockUpDTO) o;
-        return Objects.equals(id, mockup.id);
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		MockUpDTO mockup = (MockUpDTO) o;
+		return Objects.equals(id, mockup.id);
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, degree, semester, subjects);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name, degree, semester, subjects);
+	}
 
-    @Override
-    public String toString() {
-        return "Mockup{" +
-                "\n\tid: " + id +
-                ", \n\tname: " + name +
-                ", \n\tdegree: " + degree +
-                ", \n\tsemester: " + semester +
-                ", \n\tsubjects: " + subjects +
-                "\n}";
-    }
+	@Override
+	public String toString() {
+		return "Mockup{" +
+				"\n\tid: " + id +
+				", \n\tname: " + name +
+				", \n\tdegree: " + degree +
+				", \n\tsemester: " + semester +
+				", \n\tsubjects: " + subjects +
+				"\n}";
+	}
 }
