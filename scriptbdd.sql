bcd4763565f025c820808e65e75ebd8366ccd5bb
Antonin Raathaux <antoninrathaux@gmail.com>
2022-12-11 16:52:30 +0100

    Ajout du script sql

--- scriptbdd.sql
+++ scriptbdd.sql
@@ -1,5 +1,5 @@
 #------------------------------------------------------------
-#        Script MySQL. EXAM-PLANNEUR
+#               Script MySQL. EXAM-PLANNEUR
 #------------------------------------------------------------
 
 #------------------------------------------------------------
@@ -7,8 +7,7 @@
 #------------------------------------------------------------
 
 CREATE TABLE Department(
-                           name Varchar (50) NOT NULL
-    ,CONSTRAINT Department_PK PRIMARY KEY (name)
+    name Varchar(50) NOT NULL PRIMARY KEY
 )ENGINE=InnoDB;
 
 
@@ -17,8 +16,7 @@ CREATE TABLE Department(
 #------------------------------------------------------------
 
 CREATE TABLE Subject(
-                        nameSubject Varchar (50) NOT NULL
-    ,CONSTRAINT Subject_PK PRIMARY KEY (nameSubject)
+    name Varchar(50) NOT NULL PRIMARY KEY
 )ENGINE=InnoDB;
 
 
@@ -26,15 +24,15 @@ CREATE TABLE Subject(
 # Table: MockUp
 #------------------------------------------------------------
 
-CREATE TABLE MockUp(
-                       nameMockUp Varchar (50) NOT NULL ,
-                       degree     Enum ('Master') NOT NULL ,
-                       year       Enum ('One','Two','Three') NOT NULL ,
-                       semester   Enum ('One','Two') NOT NULL ,
-                       name       Varchar (50) NOT NULL
-    ,CONSTRAINT MockUp_PK PRIMARY KEY (nameMockUp)
+CREATE TABLE MockUp
+(
+    name           Varchar(50)                NOT NULL PRIMARY KEY,
+    degree         Enum ('Master')            NOT NULL,
+    year           Enum ('One','Two','Three') NOT NULL,
+    semester       Enum ('One','Two')         NOT NULL,
+    DepartmentName Varchar(50)                NOT NULL,
 
-    ,CONSTRAINT MockUp_Department_FK FOREIGN KEY (name) REFERENCES Department(name)
+    CONSTRAINT MockUp_Department_FK FOREIGN KEY (name) REFERENCES Department (name)
 )ENGINE=InnoDB;
 
 
@@ -42,12 +40,13 @@ CREATE TABLE MockUp(
 # Table: Slot
 #------------------------------------------------------------
 
-CREATE TABLE Slot(
-                     idSlot   Char (36) NOT NULL ,
-                     day      Date NOT NULL ,
-                     hour     Float NOT NULL ,
-                     duration Float NOT NULL
-    ,CONSTRAINT Slot_PK PRIMARY KEY (idSlot)
+CREATE TABLE Slot
+(
+    idSlot   Char(36) NOT NULL PRIMARY KEY,
+    day      Date     NOT NULL,
+    hour     Float    NOT NULL,
+    duration Float    NOT NULL
+
 )ENGINE=InnoDB;
 
 
@@ -55,15 +54,15 @@ CREATE TABLE Slot(
 # Table: Exam
 #------------------------------------------------------------
 
-CREATE TABLE Exam(
-                     idExam   Char (36) NOT NULL ,
-                     name     Varchar (50) NOT NULL ,
-                     duration Float NOT NULL ,
-                     type     Enum ('FinalExam') NOT NULL ,
-                     idSlot   Char (36)
-    ,CONSTRAINT Exam_PK PRIMARY KEY (idExam)
+CREATE TABLE Exam
+(
+    idExam   Char(36)           NOT NULL PRIMARY KEY,
+    name     Varchar(50)        NOT NULL,
+    duration Float              NOT NULL,
+    type     Enum ('FinalExam') NOT NULL,
+    idSlot   Char(36),
 
-    ,CONSTRAINT Exam_Slot_FK FOREIGN KEY (idSlot) REFERENCES Slot(idSlot)
+    CONSTRAINT Exam_Slot_FK FOREIGN KEY (idSlot) REFERENCES Slot (idSlot)
 )ENGINE=InnoDB;
 
 
@@ -71,15 +70,15 @@ CREATE TABLE Exam(
 # Table: Manager
 #------------------------------------------------------------
 
-CREATE TABLE Manager(
-                        id        Char (36) NOT NULL ,
-                        civility  Enum ('Men','Women','Other') NOT NULL ,
-                        lastname  Varchar (50) NOT NULL ,
-                        firstname Varchar (50) NOT NULL ,
-                        idExam    Char (36)
-    ,CONSTRAINT Manager_PK PRIMARY KEY (id)
+CREATE TABLE Manager
+(
+    id        Char(36)                     NOT NULL PRIMARY KEY,
+    civility  Enum ('Men','Women','Other') NOT NULL,
+    lastname  Varchar(50)                  NOT NULL,
+    firstname Varchar(50)                  NOT NULL,
+    idExam    Char(36),
 
-    ,CONSTRAINT Manager_Exam_FK FOREIGN KEY (idExam) REFERENCES Exam(idExam)
+    CONSTRAINT Manager_Exam_FK FOREIGN KEY (idExam) REFERENCES Exam (idExam)
 )ENGINE=InnoDB;
 
 
@@ -87,16 +86,16 @@ CREATE TABLE Manager(
 # Table: Room
 #------------------------------------------------------------
 
-CREATE TABLE Room(
-                     name   Varchar (50) NOT NULL ,
-                     places Int NOT NULL ,
-                     type   Enum ('Salle') NOT NULL ,
-                     idSlot Char (36) ,
-                     idExam Char (36)
-    ,CONSTRAINT Room_PK PRIMARY KEY (name)
+CREATE TABLE Room
+(
+    name   Varchar(50)    NOT NULL PRIMARY KEY,
+    places Int            NOT NULL,
+    type   Enum ('Salle') NOT NULL,
+    idSlot Char(36),
+    idExam Char(36),
 
-    ,CONSTRAINT Room_Slot_FK FOREIGN KEY (idSlot) REFERENCES Slot(idSlot)
-    ,CONSTRAINT Room_Exam0_FK FOREIGN KEY (idExam) REFERENCES Exam(idExam)
+    CONSTRAINT Room_Slot_FK FOREIGN KEY (idSlot) REFERENCES Slot (idSlot),
+    CONSTRAINT Room_Exam0_FK FOREIGN KEY (idExam) REFERENCES Exam (idExam)
 )ENGINE=InnoDB;
 
 
@@ -104,15 +103,16 @@ CREATE TABLE Room(
 # Table: User
 #------------------------------------------------------------
 
-CREATE TABLE User(
-                     id         Char (36) NOT NULL ,
-                     mail       Varchar (100) NOT NULL ,
-                     name       Varchar (50) ,
-                     id_Manager Char (36)
-    ,CONSTRAINT User_PK PRIMARY KEY (id)
+CREATE TABLE User
+(
+    id         Char(36)     NOT NULL PRIMARY KEY,
+    mail       Varchar(100) NOT NULL,
+    name       Varchar(50),
+    id_Manager Char(36),
 
-    ,CONSTRAINT User_Department_FK FOREIGN KEY (name) REFERENCES Department(name)
-    ,CONSTRAINT User_Manager0_FK FOREIGN KEY (id_Manager) REFERENCES Manager(id)
+
+    CONSTRAINT User_Department_FK FOREIGN KEY (name) REFERENCES Department (name),
+    CONSTRAINT User_Manager0_FK FOREIGN KEY (id_Manager) REFERENCES Manager (id)
 )ENGINE=InnoDB;
 
 
@@ -120,15 +120,15 @@ CREATE TABLE User(
 # Table: _Group
 #------------------------------------------------------------
 
-CREATE TABLE _Group(
-                       idGroup                           Char (36) NOT NULL ,
-                       name                              Varchar (50) NOT NULL ,
-                       containReductedMobilityPerson     Bool NOT NULL ,
-                       personsWithoutAdjustmentNumber    Int NOT NULL ,
-                       numberOfStudentsWithWritingNeeds  Int NOT NULL ,
-                       numberOfStudentsWithIsolatedRooms Int NOT NULL ,
-                       numberOfStudentsWithPartTime      Int NOT NULL
-    ,CONSTRAINT _Group_PK PRIMARY KEY (idGroup)
+CREATE TABLE _Group
+(
+    idGroup                           Char(36)    NOT NULL PRIMARY KEY,
+    name                              Varchar(50) NOT NULL,
+    containReductedMobilityPerson     Bool        NOT NULL,
+    personsWithoutAdjustmentNumber    Int         NOT NULL,
+    numberOfStudentsWithWritingNeeds  Int         NOT NULL,
+    numberOfStudentsWithIsolatedRooms Int         NOT NULL,
+    numberOfStudentsWithPartTime      Int         NOT NULL
 )ENGINE=InnoDB;
 
 
@@ -136,13 +136,14 @@ CREATE TABLE _Group(
 # Table: SubjectToMockUp
 #------------------------------------------------------------
 
-CREATE TABLE SubjectToMockUp(
-                                nameSubject Varchar (50) NOT NULL ,
-                                nameMockUp  Varchar (50) NOT NULL
-    ,CONSTRAINT SubjectToMockUp_PK PRIMARY KEY (nameSubject,nameMockUp)
+CREATE TABLE SubjectToMockUp
+(
+    nameSubject Varchar(50) NOT NULL,
+    nameMockUp  Varchar(50) NOT NULL,
+    CONSTRAINT SubjectToMockUp_PK PRIMARY KEY (nameSubject, nameMockUp),
 
-    ,CONSTRAINT SubjectToMockUp_Subject_FK FOREIGN KEY (nameSubject) REFERENCES Subject(nameSubject)
-    ,CONSTRAINT SubjectToMockUp_MockUp0_FK FOREIGN KEY (nameMockUp) REFERENCES MockUp(nameMockUp)
+    CONSTRAINT SubjectToMockUp_Subject_FK FOREIGN KEY (nameSubject) REFERENCES Subject (name),
+    CONSTRAINT SubjectToMockUp_MockUp0_FK FOREIGN KEY (nameMockUp) REFERENCES MockUp (name)
 )ENGINE=InnoDB;
 
 
@@ -150,13 +151,14 @@ CREATE TABLE SubjectToMockUp(
 # Table: previousExam
 #------------------------------------------------------------
 
-CREATE TABLE previousExam(
-                             idExam              Char (36) NOT NULL ,
-                             idExam_previousExam Char (36) NOT NULL
-    ,CONSTRAINT previousExam_PK PRIMARY KEY (idExam,idExam_previousExam)
+CREATE TABLE previousExam
+(
+    idExam              Char(36) NOT NULL,
+    idExam_previousExam Char(36) NOT NULL,
+    CONSTRAINT previousExam_PK PRIMARY KEY (idExam, idExam_previousExam),
 
-    ,CONSTRAINT previousExam_Exam_FK FOREIGN KEY (idExam) REFERENCES Exam(idExam)
-    ,CONSTRAINT previousExam_Exam0_FK FOREIGN KEY (idExam_previousExam) REFERENCES Exam(idExam)
+    CONSTRAINT previousExam_Exam_FK FOREIGN KEY (idExam) REFERENCES Exam (idExam),
+    CONSTRAINT previousExam_Exam0_FK FOREIGN KEY (idExam_previousExam) REFERENCES Exam (idExam)
 )ENGINE=InnoDB;
 
 
@@ -164,13 +166,14 @@ CREATE TABLE previousExam(
 # Table: ExamToDepartment
 #------------------------------------------------------------
 
-CREATE TABLE ExamToDepartment(
-                                 idExam Char (36) NOT NULL ,
-                                 name   Varchar (50) NOT NULL
-    ,CONSTRAINT ExamToDepartment_PK PRIMARY KEY (idExam,name)
+CREATE TABLE ExamToDepartment
+(
+    idExam         Char(36)    NOT NULL,
+    DepartmentName Varchar(50) NOT NULL,
+    CONSTRAINT ExamToDepartment_PK PRIMARY KEY (idExam, DepartmentName),
 
-    ,CONSTRAINT ExamToDepartment_Exam_FK FOREIGN KEY (idExam) REFERENCES Exam(idExam)
-    ,CONSTRAINT ExamToDepartment_Department0_FK FOREIGN KEY (name) REFERENCES Department(name)
+    CONSTRAINT ExamToDepartment_Exam_FK FOREIGN KEY (idExam) REFERENCES Exam (idExam),
+    CONSTRAINT ExamToDepartment_Department0_FK FOREIGN KEY (DepartmentName) REFERENCES Department (name)
 )ENGINE=InnoDB;
 
 
@@ -178,12 +181,13 @@ CREATE TABLE ExamToDepartment(
 # Table: ExamToGroup
 #------------------------------------------------------------
 
-CREATE TABLE ExamToGroup(
-                            idExam  Char (36) NOT NULL ,
-                            idGroup Char (36) NOT NULL
-    ,CONSTRAINT ExamToGroup_PK PRIMARY KEY (idExam,idGroup)
+CREATE TABLE ExamToGroup
+(
+    idExam  Char(36) NOT NULL,
+    idGroup Char(36) NOT NULL,
+    CONSTRAINT ExamToGroup_PK PRIMARY KEY (idExam, idGroup),
 
-    ,CONSTRAINT ExamToGroup_Exam_FK FOREIGN KEY (idExam) REFERENCES Exam(idExam)
-    ,CONSTRAINT ExamToGroup__Group0_FK FOREIGN KEY (idGroup) REFERENCES _Group(idGroup)
+    CONSTRAINT ExamToGroup_Exam_FK FOREIGN KEY (idExam) REFERENCES Exam (idExam),
+    CONSTRAINT ExamToGroup__Group0_FK FOREIGN KEY (idGroup) REFERENCES _Group (idGroup)
 )ENGINE=InnoDB;
 
