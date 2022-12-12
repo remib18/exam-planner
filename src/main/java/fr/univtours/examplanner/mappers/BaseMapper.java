693c2729d29e3c1132b0bfdfdedc528c0a935431
KalityGabi <115479474+KalityGabi@users.noreply.github.com>
2022-12-12 18:07:52 +0100

    Update src/main/java/fr/univtours/examplanner/mappers/BaseMapper.java
    Co-authored-by: Rémi Bernard <74245760+remib18@users.noreply.github.com>

--- src/main/java/fr/univtours/examplanner/mappers/BaseMapper.java
+++ src/main/java/fr/univtours/examplanner/mappers/BaseMapper.java
@@ -18,11 +18,12 @@ public interface BaseMapper {
         throw new UnsupportedOperationException();
     }
 
-    //Todo(@gab): same
     /**
      * Permet de transformer les attributs/données d'une classe (DTO) en données (entity) exploitables dans une requête.
+     *
      * @param DTO Correspond à la classe dont certaines données seront utilisées dans une requête
      * @return c'est une methode d'interface, mais l'IDE ne supporte pas qu'il n'y a pas de "body"
+     *
      * @param <Entity> On indique une classe générique pour pouvoir utiliser la même méthode sur différentes entités
      * @param <DTO> On indique une classe générique pour pouvoir utiliser la même méthode sur différents DTO
      */
