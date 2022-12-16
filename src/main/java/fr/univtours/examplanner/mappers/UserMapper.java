34ceead5aa94caa3ce6f0c0a41d82f8eec3e1576
Benoît Cheramy <95913165+beubeu28@users.noreply.github.com>
2022-12-16 17:25:28 +0100

    Update src/main/java/fr/univtours/examplanner/mappers/UserMapper.java

--- src/main/java/fr/univtours/examplanner/mappers/UserMapper.java
+++ src/main/java/fr/univtours/examplanner/mappers/UserMapper.java
@@ -2,7 +2,7 @@ package fr.univtours.examplanner.mappers;
 
 import fr.univtours.examplanner.entities.dtos.UserDTO;
 import org.jetbrains.annotations.NotNull;
-
+import java.sql.PreparedStatement
 import java.sql.ResultSet;
 
 public class UserMapper implements BaseMapper{
